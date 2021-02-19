d0aa4f36e77b3f88e8014e7d35129248dbc53081
MrGeoTech (Isaac) <MrGeoTech (Isaac)@LAPTOP-I4C6H9BV>
2021-02-19 17:09:59 -0600

    Working on command listeners

--- src/main/java/me/mrgeotech/commands/CommandListener.java
+++ src/main/java/me/mrgeotech/commands/CommandListener.java
@@ -1,17 +1,20 @@
 package me.mrgeotech.commands;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.server.ServerCommandEvent;
 
 import me.mrgeotech.main.UUIDFetcher;
 import me.mrgeotech.main.UniversalHistory;
@@ -30,91 +33,78 @@ public class CommandListener implements Listener {
 	}
 	
 	@EventHandler
-	public void onPlayerCommand(PlayerCommandPreprocessEvent e) {
-		String[] sections = e.getMessage().split("");
-		switch (sections[0].toLowerCase()) {
-		case "kick":
-			if (sections.length == 3) {
-				this.sendPunishment(sections[1], e.getPlayer(), sections[2], "Kick");
-			} else if (sections.length == 2) {
-				this.sendPunishment(sections[1], e.getPlayer(), "No reason given", "Kick");
-			}
-		case "warn":
-			if (sections.length == 3) {
-				this.sendPunishment(sections[1], e.getPlayer(), sections[2], "Warning");
-			} else if (sections.length == 2) {
-				this.sendPunishment(sections[1], e.getPlayer(), "No reason given", "Warning");
-			}
-		case "mute":
-			if (sections.length == 3) {
-				this.sendPunishment(sections[1], e.getPlayer(), sections[2], "Mute");
-			} else if (sections.length == 2) {
-				this.sendPunishment(sections[1], e.getPlayer(), "No reason given", "Mute");
-			}
-		case "tempmute":
-			if (sections.length == 4) {
-				this.sendPunishment(sections[1], e.getPlayer(), sections[2], "Temp Mute", sections[3]);
-			} else if (sections.length == 3) {
-				this.sendPunishment(sections[1], e.getPlayer(), "No reason given", "Temp Mute", sections[2]);
-			}
-		case "ipmute":
-			if (sections.length == 3) {
-				this.sendPunishment(sections[1], e.getPlayer(), sections[2], "IP Mute");
-			} else if (sections.length == 2) {
-				this.sendPunishment(sections[1], e.getPlayer(), "No reason given", "IP Mute");
-			}
-		case "ban":
-			if (sections.length == 3) {
-				this.sendPunishment(sections[1], e.getPlayer(), sections[2], "Ban");
-			} else if (sections.length == 2) {
-				this.sendPunishment(sections[1], e.getPlayer(), "No reason given", "Ban");
-			}
-		case "tempban":
-			if (sections.length == 4) {
-				this.sendPunishment(sections[1], e.getPlayer(), sections[2], "Temp Ban", sections[3]);
-			} else if (sections.length == 3) {
-				this.sendPunishment(sections[1], e.getPlayer(), "No reason given", "Temp Ban", sections[2]);
-			}
-		case "ipban":
-			if (sections.length == 3) {
-				this.sendPunishment(sections[1], e.getPlayer(), sections[2], "IP Ban");
-			} else if (sections.length == 2) {
-				this.sendPunishment(sections[1], e.getPlayer(), "No reason given", "IP Ban");
-			}
-		case "tempipban":
-			if (sections.length == 4) {
-				this.sendPunishment(sections[1], e.getPlayer(), sections[2], "Temp IP Ban", sections[3]);
-			} else if (sections.length == 3) {
-				this.sendPunishment(sections[1], e.getPlayer(), "No reason given", "Temp IP Ban", sections[2]);
-			}
+	public void commandListener(ServerCommandEvent e) {
+		ArrayList<String> sections = new ArrayList<String>();
+		for (String s : e.getCommand().split(" ")) {
+			sections.add(s);
 		}
+		System.out.println(sections);
+		String command = sections.get(0);
+		if (sections.get(1).equalsIgnoreCase("-s")) {
+			System.out.println("True");
+			sections.remove(1);
+		}
+		String playerName = sections.get(1);
+		sections.remove(0);
+		sections.remove(0);
+		System.out.println(sections);
+		String reason = sections.get(0);
+		sections.remove(0);
+		for (String s : sections) {
+			reason.concat(" " + s);
+		}
+		System.out.println(command + " : " + playerName + " : " + reason);
 	}
-	
-	private void sendPunishment(final String player, final Player staff, final String reason, final String type) {
+
+	public void sendPunishment(final String player, final CommandSender sender, final String reason, final String type, final String length) {
 		Bukkit.getScheduler().runTaskAsynchronously(main, new Runnable() {
 			@Override
 			public void run() {
-				try {
-					String uuid;
+				if (sender instanceof Player) {
+					Player staff = (Player) sender;
 					try {
-						uuid = UUIDFetcher.getUUIDOf(player).toString();
-					} catch (Exception e) {
-						System.err.println(staff.getName() + " tried to get the uuid of " + player + " which could not be found/doesn't exist!");
-						staff.sendMessage(ChatColor.RED + player + "'s uuid could not be found/doesn't exist!");
-						return;
+						String uuid;
+						try {
+							uuid = UUIDFetcher.getUUIDOf(player).toString();
+						} catch (Exception e) {
+							System.err.println(staff.getName() + " tried to get the uuid of " + player + " which could not be found/doesn't exist!");
+							staff.sendMessage(ChatColor.RED + player + "'s uuid could not be found/doesn't exist!");
+							return;
+						}
+						Socket server = new Socket(IP, PORT);
+						ObjectOutputStream outStream = new ObjectOutputStream(server.getOutputStream());
+						ObjectInputStream inStream = new ObjectInputStream(server.getInputStream());
+						String[] out = {"add", uuid, player, staff.getUniqueId().toString(), staff.getName(), Bukkit.getIp(), ZonedDateTime.ofInstant(Instant.now(), ZoneId.of("America/Chicago")).toString(), type + "(Length=" + length + ")", reason};
+						outStream.writeObject(out);
+						String response = inStream.readUTF();
+						staff.sendMessage(response);
+						server.close();
+					} catch (IOException e) {
+						e.printStackTrace();
 					}
-					Socket server = new Socket(IP, PORT);
-					ObjectOutputStream outStream = new ObjectOutputStream(server.getOutputStream());
-					String[] out = {"add", uuid, player, staff.getUniqueId().toString(), staff.getName(), Bukkit.getIp(), ZonedDateTime.ofInstant(Instant.now(), ZoneId.of("America/Chicago")).toString(), type, reason};
-				} catch (IOException e) {
-					e.printStackTrace();
+				} else {
+					try {
+						String uuid;
+						try {
+							uuid = UUIDFetcher.getUUIDOf(player).toString();
+						} catch (Exception e) {
+							System.err.println("Console tried to get the uuid of " + player + " which could not be found/doesn't exist!");
+							sender.sendMessage(ChatColor.RED + player + "'s uuid could not be found/doesn't exist!");
+							return;
+						}
+						Socket server = new Socket(IP, PORT);
+						ObjectOutputStream outStream = new ObjectOutputStream(server.getOutputStream());
+						ObjectInputStream inStream = new ObjectInputStream(server.getInputStream());
+						String[] out = {"add", uuid, player, "Console", "Console", Bukkit.getIp(), ZonedDateTime.ofInstant(Instant.now(), ZoneId.of("America/Chicago")).toString(), type + "(Length=" + length + ")", reason};
+						outStream.writeObject(out);
+						String response = inStream.readUTF();
+						sender.sendMessage(response);
+						server.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
 				}
 			}
 		});
 	}
-
-	private void sendPunishment(final String player, final Player staff, final String reason, final String type, final String length) {
-		
-	}
-	
 }
--- src/main/java/me/mrgeotech/main/UniversalHistory.java
+++ src/main/java/me/mrgeotech/main/UniversalHistory.java
@@ -5,16 +5,20 @@ import java.net.ServerSocket;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.mrgeotech.commands.CommandListener;
 import me.mrgeotech.commands.UniversalHistoryCommand;
 
 public class UniversalHistory extends JavaPlugin {
 	
 	@SuppressWarnings("unused")
 	private UniversalHistoryCommand uhc;
+	@SuppressWarnings("unused")
+	private CommandListener cl;
 	
 	@Override
 	public void onEnable() {
 		uhc = new UniversalHistoryCommand(this);
+		cl = new CommandListener(this);
 	}
 	
 	@Override
